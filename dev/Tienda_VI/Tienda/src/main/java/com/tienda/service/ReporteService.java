7d2d7c8c5a9b0aa9296fbbcd405d61e6f5c431d1
Brians Fernando <fervargas2003@hotmail.com>
2023-12-06 15:54:26 -0600

    Semana#12
    Reportes

---
+++ dev/Tienda_VI/Tienda/src/main/java/com/tienda/service/ReporteService.java
@@ -0,0 +1,20 @@
+package com.tienda.service;
+
+
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+/**
+ *
+ * @author ferva
+ */
+public interface ReporteService {
+
+    public ResponseEntity<Resource> generaReporte(
+            String reporte,
+            Map<String, Object> parametros,
+            String tipo) throws IOException;
+
+}
